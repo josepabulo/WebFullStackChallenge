b5733dcac5fb8d2f4066c27eac5962686860fc76
José Pábulo <jose.castro@citsamazonas.org.br>
2021-04-14 02:35:04 -0400

    feat: página inicial simples para o sistema

--- front-end/src/app/view/home/home.module.ts
+++ front-end/src/app/view/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MaterialModule
   ]
 })
 export class HomeModule { }
