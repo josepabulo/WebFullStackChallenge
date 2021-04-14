6ee1e4e3d11c66294af6776ebccea1b6deff36bb
José Pábulo <jose.castro@citsamazonas.org.br>
2021-04-14 01:01:59 -0400

    fix: solução para problema de cors na conexão do front

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,12 @@
 
     app.use(express.json())
 
+    const cors = require('cors')
+    var corsOptions = {
+      origin: '*'
+    }
+    app.use(cors(corsOptions))
+
     app.use('/', require('./src/routes/index.js'))
 
     const db = require('./config/db')
